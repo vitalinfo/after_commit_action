a04076f747cada581a6960b309fb72861352a1ba
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2016-06-02 13:56:22 +0000

    Add activerecord config

--- lib/after_commit_action.rb
+++ lib/after_commit_action.rb
@@ -1,6 +1,7 @@
 require 'active_record'
 
 if defined?(ActiveRecord::Base)
+  ActiveRecord::Base.raise_in_transactional_callbacks = true
   require 'after_commit_action/base'
   ActiveRecord::Base.include AfterCommitAction
 end
