c84bfb8a6109bd75d0a378deec9c1ae08626f60c
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2016-12-07 09:39:55 +0000

    Remove Rails 5 DEPRECATION WARNING

--- lib/after_commit_action.rb
+++ lib/after_commit_action.rb
@@ -1,7 +1,7 @@
 require 'active_record'
 
 if defined?(ActiveRecord::Base)
-  ActiveRecord::Base.raise_in_transactional_callbacks = true
+  ActiveRecord::Base.raise_in_transactional_callbacks = true if ActiveRecord::VERSION::MAJOR < 5
   require 'after_commit_action/base'
   ActiveRecord::Base.include AfterCommitAction
 end
